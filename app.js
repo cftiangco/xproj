fa13d6bb65c6ca0f360d54a2e44ec820d34e871a
Crimson Tiangco <crimson.tiangco.ipi@prulifeuk.com.ph>
2023-03-20 22:11:10 +0800

    test docker volume

--- app.js
+++ app.js
@@ -1,30 +1,38 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const Person = require('./models/Person')
+const bodyParser = require('body-parser')
+
+
 const app  = express();
 
 const PORT =  3000;
 
 
-mongoose.connect('mongodb://mydb/person')
+mongoose.connect(`mongodb://mydb/person`,{ useNewUrlParser: true })
   .then(() => console.log('DB connected!'))
   .catch((err) => console.log(`err`,err));
 
-app.get('/', async (req,res) =>  {
+
+app.use(bodyParser.urlencoded({ extended: false }))  
+app.use(bodyParser.json())
+
+
+app.get('/api/persons', async (req,res) =>  {
     const person = await Person.find({});
     console.log(`person`,person);
     res.send(person);
 });
 
-app.get('/add', async (req,res) => {
+app.post('/api/persons', async (req,res) => {
 
     const newPerson = {
-        name:req.query.name
+        name:req.body.name
     }
 
     try {
         const result = await Person.create(newPerson);
-        res.send(result);
+        res.status(201).send(result);
     } catch(e) {
         console.log(`error:`,e);
         res.send(e);
